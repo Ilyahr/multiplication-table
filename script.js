2ce836c78701676f6692606b3b664b351039550c
admin <admin@MacBook-Pro-admin.local>
2020-05-19 20:11:34 +0300

    feat: create animation and alot feat

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 let form = document.getElementById("form"),
     monoDigit = document.querySelectorAll(".single-digit"),
     duoDigit = document.querySelectorAll(".complex-task"),
-    task = document.getElementById('task'),
+    anim = document.querySelector(".work-area"),
+    task = document.querySelector('.task'),
     result = document.getElementById('result'),
-    answer = document.getElementById('answer'),
+    win = document.querySelector(".true-pointer"),
+    loose = document.querySelector(".false-pointer"),
     firstMultiplier = 1,
     secMultiplier = 1;
 let POINTER = 0,
@@ -14,14 +16,43 @@ let POINTER = 0,
     fine = 0;
 
 
+// choose task
+
+monoDigit.forEach(function (item, i ) {
+    item.addEventListener("click", function () {
+        miss = 0;
+        fine = 0;
+        TASKNUM = i;
+        result.focus();
+        digit = 0;
+        createTask(TASKNUM, digit);
+
+    });
+});
+
+duoDigit.forEach(function (item, i ) {
+    item.addEventListener("click", function () {
+        miss = 0;
+        fine = 0;
+        TASKNUM = i;
+        result.focus();
+        digit = 1;
+        createTask(TASKNUM, digit);
+
+    });
+});
+
+// create random element
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 
+// output the task on the page
+
 function createTask(i, digit) {
     task.value = "";
-    answer.value = "";
     result.value = "";
     if (digit === 0) {
         firstMultiplier = i + 1;
@@ -32,53 +63,119 @@ function createTask(i, digit) {
         secMultiplier = getRandomInt(11);
     }
 
-    task.value = firstMultiplier + " * " + secMultiplier;
-    console.log(firstMultiplier + "*" + secMultiplier);
+    task.innerHTML = firstMultiplier + " * " + secMultiplier;
 }
-form.addEventListener( "submit", function (e) {
+
+// wait user answer
+
+form.addEventListener( "submit", function (e){
+
     e.preventDefault();
-    console.log("зашел в событие");
-    if (+result.value === firstMultiplier * secMultiplier) {
-        answer.value = "прекрасно";
-        setTimeout(function (){ console.log("я тут"); createTask(TASKNUM, digit);}, 1000);
+
+    if (+result.value === firstMultiplier * secMultiplier){
+
+        fine++;
+
+        win.innerHTML = fine;
+
+        drawFront(true);
+
+        setTimeout(function (){
+            drawBack(true);
+            createTask(TASKNUM, digit);
+        }, 600);
     }
+
     else{
-        console.log("ошибка");
-        answer.value = "Попробуй еще раз";
+        miss++;
+
+        loose.innerHTML = miss;
+
+        drawFront(false);
+
         setTimeout(function (){
             result.value = "";
-            result.focus();}, 1000);
+            drawBack(false);
+            result.focus();
+        }, 600);
     }
-}, );
-
-monoDigit.forEach(function (item, i ) {
-    item.addEventListener("click", function () {
-        miss = 0;
-        fine = 0;
-        TASKNUM = i;
-        result.focus();
-        console.log("нажал так-то на " + i + "кнопку" );
-        digit = 0;
-        createTask(TASKNUM, digit);
-
-        });
-
 });
 
-duoDigit.forEach(function (item, i ) {
-    item.addEventListener("click", function () {
-        miss = 0;
-        fine = 0;
-        TASKNUM = i;
-        result.focus();
-        console.log("нажал так-то на " + i + "кнопку" );
-        digit = 1;
-        createTask(TASKNUM, digit);
+
+
+
+
+
+
+
+function makeEaseOut(timing) {
+    return function(timeFraction) {
+        return 1 - timing(1 - timeFraction);
+    }
+}
+
+function bounce(timeFraction) {
+    for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+            return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+    }
+}
+
+function quad(timeFraction) {
+    return Math.pow(timeFraction, 2)
+}
+
+function drawFront(react) {
+    animate({
+        duration: 600,
+        timing: makeEaseOut(bounce),
+        draw(progress) {
+            if(!react)
+                anim.style.gridTemplateColumns = 50 + 50 * progress + "% " + (50 - 50 * progress) + "%";
+            else
+                anim.style.gridTemplateColumns = (50 - 50 * progress) + "% " + 50 + 50 * progress + "%";
+        }
+    });
+}
+
+
+
+
+function drawBack(react) {
+    animate({
+        duration: 400,
+        timing: makeEaseOut(quad),
+        draw(progress) {
+            if(!react)
+                anim.style.gridTemplateColumns = 100 - 50 * progress + "% " + (50 * progress) + "%";
+            else
+                anim.style.gridTemplateColumns = (50 * progress) + "% " + (100 - 50 * progress) + "%";
+        }
+    });
+}
+
+function animate(options) {
+
+    var start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+
+        // timeFraction от 0 до 1
+        var timeFraction = (time - start) / options.duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        // текущее состояние анимации
+        var progress = options.timing(timeFraction);
+
+        options.draw(progress);
+
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
 
     });
-
-});
-
+}
 
 
 
